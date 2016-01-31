7ab270a10f468f0ae96ce8d05b55fe3f9f4981a0
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:06:53 -0500

    one test written - currently failing on import of converter

--- number_converter_test.py
+++ number_converter_test.py
@@ -1,8 +1,12 @@
 import unittest
+import number_converter
 
 
 class NumberConverterTest(unittest.TestCase):
-    pass
+
+    def test_that_one_is_i(self):
+        converter = number_converter.NumberConverter()
+        self.assertEqual(converter.convert(1), "I")
 
 if __name__ == '__main__':
     unittest.main()
