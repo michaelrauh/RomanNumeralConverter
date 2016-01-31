6772908683cabd5faacb16b6fb6b766d94a9ab90
michaelrauh <michaelrauh@ymail.com>
2016-01-30 22:39:53 -0500

    got rid of check for previous value and added the bad patterns to list as the bad patterns are very limited

--- number_converter.py
+++ number_converter.py
@@ -16,21 +16,18 @@ class NumberConverter():
         numeral_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         numeral_to_arabic_subtract_patterns = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
 
-        illegal_patterns = ["IIII", "XXXX", "CCCC", "MMMM", "VV", "LL", "DD", "IXL", "IXC", "XCD", "XCM"]
+        illegal_patterns = ["IIII", "XXXX", "CCCC", "MMMM", "VV", "LL", "DD", "IXL", "IXC", "XCD", "XCM", "IIV", "IIX",
+                            "XXL", "XXC", "CCD", "CCM"]
         for pattern in illegal_patterns:
             if pattern in numeral:
                 return 0
 
         arabic = 0
-        previous = float("inf")
         while numeral != "":
             if numeral[:2] in numeral_to_arabic_subtract_patterns:
                 arabic += numeral_to_arabic_subtract_patterns[numeral[:2]]
-                if previous < numeral_to_arabic_subtract_patterns[numeral[:2]]:
-                    return 0
                 numeral = numeral[2:]
             else:
                 arabic += numeral_to_arabic[numeral[0]]
                 numeral = numeral[1:]
-            previous = arabic
         return arabic
