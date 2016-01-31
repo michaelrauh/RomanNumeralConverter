efd7b412bf688efb309006ce07d65f4eb65b10b6
michaelrauh <michaelrauh@ymail.com>
2016-01-30 22:45:01 -0500

    moved illegal pattern check into helper method

--- number_converter.py
+++ number_converter.py
@@ -16,11 +16,8 @@ class NumberConverter():
         numeral_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         numeral_to_arabic_subtract_patterns = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
 
-        illegal_patterns = ["IIII", "XXXX", "CCCC", "MMMM", "VV", "LL", "DD", "IXL", "IXC", "XCD", "XCM", "IIV", "IIX",
-                            "XXL", "XXC", "CCD", "CCM"]
-        for pattern in illegal_patterns:
-            if pattern in numeral:
-                return 0
+        if NumberConverter.check_for_illegal_pattern(numeral):
+            return 0
 
         arabic = 0
         while numeral != "":
@@ -31,3 +28,11 @@ class NumberConverter():
                 arabic += numeral_to_arabic[numeral[0]]
                 numeral = numeral[1:]
         return arabic
+
+    @staticmethod
+    def check_for_illegal_pattern(numeral):
+        illegal_patterns = ["IIII", "XXXX", "CCCC", "MMMM", "VV", "LL", "DD", "IXL", "IXC", "XCD", "XCM", "IIV", "IIX",
+                            "XXL", "XXC", "CCD", "CCM"]
+        for pattern in illegal_patterns:
+            if pattern in numeral:
+                return True
