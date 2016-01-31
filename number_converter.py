788dcdf8ad1d23fcb302bd6f30b4e7b35574ae71
michaelrauh <michaelrauh@ymail.com>
2016-01-30 21:30:19 -0500

    wrote code for basic conversion of numeral to arabic. Currently passing.

--- number_converter.py
+++ number_converter.py
@@ -13,4 +13,5 @@ class NumberConverter():
 
     @staticmethod
     def convert_numeral_to_arabic(numeral):
-        return 1
+        numeral_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        return numeral_to_arabic[numeral]
