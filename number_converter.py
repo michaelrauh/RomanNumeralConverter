e0c8186cc8e7cc93bd886efd3696b36865a38b93
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:59:24 -0500

    Added code to converter to take subtraction rule into account. This is done naively by looking at subtracting groups as a single symbol. Tests currently pass

--- number_converter.py
+++ number_converter.py
@@ -2,7 +2,8 @@ class NumberConverter():
 
     @staticmethod
     def convert(arabic):
-        arabic_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+        arabic_to_numeral = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",
+                             400: "CD", 500: "D", 900: "CM", 1000: "M"}
         numeral = ""
         while arabic > 0:
             highest_below_arabic = max([i for i in arabic_to_numeral.keys() if i <= arabic])
