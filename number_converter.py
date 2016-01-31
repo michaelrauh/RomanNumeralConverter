a6476f4f3fda191b8c1d7283597cf97e1e311fb1
michaelrauh <michaelrauh@ymail.com>
2016-01-30 22:13:00 -0500

    added code to return zero if the roman numeral contains four of the same symbol in a row. Tests currently passing.

--- number_converter.py
+++ number_converter.py
@@ -16,6 +16,11 @@ class NumberConverter():
         numeral_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         numeral_to_arabic_subtract_patterns = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
 
+        illegal_patterns = ["IIII", "XXXX", "CCCC", "MMMM"]
+        for pattern in illegal_patterns:
+            if pattern in numeral:
+                return 0
+
         arabic = 0
         while numeral != "":
             if numeral[:2] in numeral_to_arabic_subtract_patterns:
