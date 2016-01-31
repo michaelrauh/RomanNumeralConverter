c3eb7cac44533b4ca1f0d02aa7144498b1379ed1
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:48:16 -0500

    Refactored naming in convert function

--- number_converter.py
+++ number_converter.py
@@ -1,11 +1,11 @@
 class NumberConverter():
 
     @staticmethod
-    def convert(number):
-        number_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
-        final = ""
-        while number > 0:
-            nearest_key = max([i for i in number_to_numeral.keys() if i <= number])
-            final += number_to_numeral[nearest_key]
-            number -= nearest_key
-        return final
+    def convert(arabic):
+        arabic_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+        numeral = ""
+        while arabic > 0:
+            highest_below_arabic = max([i for i in arabic_to_numeral.keys() if i <= arabic])
+            numeral += arabic_to_numeral[highest_below_arabic]
+            arabic -= highest_below_arabic
+        return numeral
