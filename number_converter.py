4be39f5124f5b176ce3d276f4824117413584ea5
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:28:24 -0500

    test is passing for 1000 is M. This is all of the basic mappings, or definitions of the numerals

--- number_converter.py
+++ number_converter.py
@@ -2,5 +2,5 @@ class NumberConverter():
 
     @staticmethod
     def convert(number):
-        number_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D"}
+        number_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
         return number_to_numeral[number]
