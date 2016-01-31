185b8c62c22deb5c3b2d233445b7292037ad20b3
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:14:37 -0500

    second test is written but currently failing. Testing that five converts to V

--- number_converter_test.py
+++ number_converter_test.py
@@ -8,5 +8,9 @@ class NumberConverterTest(unittest.TestCase):
         converter = number_converter.NumberConverter()
         self.assertEqual(converter.convert(1), "I")
 
+    def test_that_five_is_V(self):
+        converter = number_converter.NumberConverter()
+        self.assertEqual(converter.convert(5), "V")
+
 if __name__ == '__main__':
     unittest.main()
