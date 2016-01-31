3449d0b4de9a742d1d9b785ec4e61f2fd7aee70f
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:18:45 -0500

    refactored code to eliminate conditional logic

--- number_converter.py
+++ number_converter.py
@@ -1,7 +1,6 @@
 class NumberConverter():
 
-    def convert(self, number):
-        if number == 1:
-            return "I"
-        else:
-            return "V"
+    @staticmethod
+    def convert(number):
+        number_to_numeral = {1: "I", 5: "V"}
+        return number_to_numeral[number]
