640276b4a1e74c44efc3c928a6f96993b511f1aa
michaelrauh <michaelrauh@ymail.com>
2016-01-30 21:15:36 -0500

    refactored name convert to convert arabic to numeral to differentiate it from the reverse conversion

--- number_converter.py
+++ number_converter.py
@@ -1,7 +1,7 @@
 class NumberConverter():
 
     @staticmethod
-    def convert(arabic):
+    def convert_arabic_to_numeral(arabic):
         arabic_to_numeral = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",
                              400: "CD", 500: "D", 900: "CM", 1000: "M"}
         numeral = ""
--- number_converter_test.py
+++ number_converter_test.py
@@ -8,38 +8,38 @@ class NumberConverterTest(unittest.TestCase):
         self.converter = number_converter.NumberConverter()
 
     def test_that_basic_numeral_definitions_work(self):
-        self.assertEqual(self.converter.convert(1), "I")
-        self.assertEqual(self.converter.convert(5), "V")
-        self.assertEqual(self.converter.convert(10), "X")
-        self.assertEqual(self.converter.convert(50), "L")
-        self.assertEqual(self.converter.convert(100), "C")
-        self.assertEqual(self.converter.convert(500), "D")
-        self.assertEqual(self.converter.convert(1000), "M")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(1), "I")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(5), "V")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(10), "X")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(50), "L")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(100), "C")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(500), "D")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(1000), "M")
 
     def test_conversion_using_basic_adding_rule(self):
-        self.assertEqual(self.converter.convert(2), "II")
-        self.assertEqual(self.converter.convert(6), "VI")
-        self.assertEqual(self.converter.convert(20), "XX")
-        self.assertEqual(self.converter.convert(1001), "MI")
-        self.assertEqual(self.converter.convert(61), "LXI")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(2), "II")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(6), "VI")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(20), "XX")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(1001), "MI")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(61), "LXI")
 
     def test_conversion_using_basic_subtraction_rule(self):
-        self.assertEqual(self.converter.convert(4), "IV")
-        self.assertEqual(self.converter.convert(9), "IX")
-        self.assertEqual(self.converter.convert(40), "XL")
-        self.assertEqual(self.converter.convert(900), "CM")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(4), "IV")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(9), "IX")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(40), "XL")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(900), "CM")
 
     def test_conversion_of_arbitrary_number(self):
-        self.assertEqual(self.converter.convert(1066), "MLXVI")
-        self.assertEqual(self.converter.convert(1989), "MCMLXXXIX")
-        self.assertEqual(self.converter.convert(3), "III")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(1066), "MLXVI")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(1989), "MCMLXXXIX")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(3), "III")
 
     def test_forbidden_numbers(self):
-        self.assertEqual(self.converter.convert(-1), "")
-        self.assertEqual(self.converter.convert(0), "")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(-1), "")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(0), "")
 
     def test_overflow_numbers(self):
-        self.assertEqual(self.converter.convert(5000), "")
+        self.assertEqual(self.converter.convert_arabic_to_numeral(5000), "")
 
 if __name__ == '__main__':
     unittest.main()
