3cbafd080092a6773b48507932d83a7cd3fe4a4f
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:26:58 -0500

    test is passing for 500 is D

--- number_converter.py
+++ number_converter.py
@@ -2,5 +2,5 @@ class NumberConverter():
 
     @staticmethod
     def convert(number):
-        number_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C"}
+        number_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D"}
         return number_to_numeral[number]
