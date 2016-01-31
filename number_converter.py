a81278bd02e57331b0cd53926f8996bbfa44b628
michaelrauh <michaelrauh@ymail.com>
2016-01-30 21:13:39 -0500

    added code to converter to prevent numbers that are too large (over 5000).

--- number_converter.py
+++ number_converter.py
@@ -5,7 +5,7 @@ class NumberConverter():
         arabic_to_numeral = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",
                              400: "CD", 500: "D", 900: "CM", 1000: "M"}
         numeral = ""
-        while arabic > 0:
+        while 0 < arabic < 5000:
             highest_below_arabic = max([i for i in arabic_to_numeral.keys() if i <= arabic])
             numeral += arabic_to_numeral[highest_below_arabic]
             arabic -= highest_below_arabic
