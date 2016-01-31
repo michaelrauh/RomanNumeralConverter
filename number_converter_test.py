ee8e5152a13ace00909b7d860ec5cd0bbadf7838
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:20:15 -0500

    tests refactored to use setup step

--- number_converter_test.py
+++ number_converter_test.py
@@ -4,13 +4,14 @@ import number_converter
 
 class NumberConverterTest(unittest.TestCase):
 
+    def setUp(self):
+        self.converter = number_converter.NumberConverter()
+
     def test_that_one_is_i(self):
-        converter = number_converter.NumberConverter()
-        self.assertEqual(converter.convert(1), "I")
+        self.assertEqual(self.converter.convert(1), "I")
 
     def test_that_five_is_V(self):
-        converter = number_converter.NumberConverter()
-        self.assertEqual(converter.convert(5), "V")
+        self.assertEqual(self.converter.convert(5), "V")
 
 if __name__ == '__main__':
     unittest.main()
