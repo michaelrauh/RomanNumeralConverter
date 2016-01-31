bb57a78a56dea2bcc43d539ce5af61564d980520
michaelrauh <michaelrauh@ymail.com>
2016-01-30 20:45:24 -0500

    test for basic addition case of two passed

--- number_converter.py
+++ number_converter.py
@@ -3,4 +3,9 @@ class NumberConverter():
     @staticmethod
     def convert(number):
         number_to_numeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
-        return number_to_numeral[number]
+        final = ""
+        while number > 0:
+            nearest_key = max([i for i in number_to_numeral.keys() if i <= number])
+            final += number_to_numeral[nearest_key]
+            number -= nearest_key
+        return final
