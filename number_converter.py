3a5b9ff907fe22422f682917249d28f010083ff1
michaelrauh <michaelrauh@ymail.com>
2016-02-08 20:58:39 -0500

    moved arabic to numeral map over to init function so it is not rebuilt each time function is called. Currently considering it a constant.

--- number_converter.py
+++ number_converter.py
@@ -1,13 +1,14 @@
 class NumberConverter():
 
-    @staticmethod
-    def convert_arabic_to_numeral(arabic):
-        arabic_to_numeral = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",
-                             400: "CD", 500: "D", 900: "CM", 1000: "M"}
+    def __init__(self):
+        self.ARABIC_TO_NUMERAL = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",
+                                  400: "CD", 500: "D", 900: "CM", 1000: "M"}
+
+    def convert_arabic_to_numeral(self, arabic):
         numeral = ""
         while 0 < arabic < 5000:
-            highest_below_arabic = max([i for i in arabic_to_numeral.keys() if i <= arabic])
-            numeral += arabic_to_numeral[highest_below_arabic]
+            highest_below_arabic = max([i for i in self.ARABIC_TO_NUMERAL.keys() if i <= arabic])
+            numeral += self.ARABIC_TO_NUMERAL[highest_below_arabic]
             arabic -= highest_below_arabic
         return numeral
 
