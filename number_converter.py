50b142f1e6674288625c698248d32e8086bda161
michaelrauh <michaelrauh@ymail.com>
2016-02-08 21:00:29 -0500

    moved numeral to arabic dictionaries over to be static and class level

--- number_converter.py
+++ number_converter.py
@@ -3,6 +3,8 @@ class NumberConverter():
     def __init__(self):
         self.ARABIC_TO_NUMERAL = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",
                                   400: "CD", 500: "D", 900: "CM", 1000: "M"}
+        self.NUMERAL_TO_ARABIC = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        self.NUMERAL_TO_ARABIC_SUBTRACT_PATTERNS = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
 
     def convert_arabic_to_numeral(self, arabic):
         numeral = ""
@@ -12,21 +14,18 @@ class NumberConverter():
             arabic -= highest_below_arabic
         return numeral
 
-    @staticmethod
-    def convert_numeral_to_arabic(numeral):
-        numeral_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        numeral_to_arabic_subtract_patterns = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+    def convert_numeral_to_arabic(self, numeral):
 
         if NumberConverter.check_for_illegal_pattern(numeral):
             return 0
 
         arabic = 0
         while numeral != "":
-            if numeral[:2] in numeral_to_arabic_subtract_patterns:
-                arabic += numeral_to_arabic_subtract_patterns[numeral[:2]]
+            if numeral[:2] in self.NUMERAL_TO_ARABIC_SUBTRACT_PATTERNS:
+                arabic += self.NUMERAL_TO_ARABIC_SUBTRACT_PATTERNS[numeral[:2]]
                 numeral = numeral[2:]
             else:
-                arabic += numeral_to_arabic[numeral[0]]
+                arabic += self.NUMERAL_TO_ARABIC[numeral[0]]
                 numeral = numeral[1:]
         return arabic
 
